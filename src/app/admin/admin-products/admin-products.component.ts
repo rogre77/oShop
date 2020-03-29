6ce78db00abacedb95ca3dcff69f0c6c920eea6e
Roger Castillon <rogre77@gmail.com>
2020-03-29 19:28:28 +1300

    2nd commit

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from './../../product.service';
 import { Subscription } from 'rxjs/internal/Subscription';
 import { Product } from 'src/app/models/product';
-import { DataTableResource } from 'angular-4-data-table';
 
 @Component({
   selector: 'app-admin-products',
@@ -13,44 +12,20 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
   products: Product[];
   filteredProducts: any[];  
   subscription: Subscription;
-  tableResource: DataTableResource<Product>;
-  items: Product[] = [];
-  itemCount: number; 
   
   
   constructor(private productService: ProductService) { 
     this.subscription = this.productService.getAll()
-      .subscribe(products => {
-        this.products = products;
-        this.initializeTable(products);
-      });
+      .subscribe(products => this.filteredProducts = this.products = products);
   }
-
-  private initializeTable(products: Product[]) {
-    this.tableResource = new DataTableResource(products);
-    this.tableResource.query({ offset: 0 })
-      .then(items => this.items = items);
-    this.tableResource.count()
-      .then(count => this.itemCount = count);
-  }
-
-  reloadItems(params) {
-    if (!this.tableResource) return;
-
-    this.tableResource.query(params)
-      .then(items => this.items = items);    
-  }
-
-  filter(query: string) { 
-    let filteredProducts = (query) ?
-      this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
-      this.products;
-
-    this.initializeTable(filteredProducts);
-  }
-
   ngOnDestroy() {
     this.subscription.unsubscribe();
+  }
+
+  filter(query: string) {
+    this.filteredProducts = (query) ?
+      this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
+      this.products;
   }
 
   ngOnInit() {
